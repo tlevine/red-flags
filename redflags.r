9f51a47634155a64c5bc8130e339347ffaddd896
Thomas Levine <_@thomaslevine.com>
2014-06-18 04:51:58 -0400

    pretty plots

--- redflags.r
+++ redflags.r
@@ -5,7 +5,7 @@ library(scales)
 
 plot.contract <- function(contract) {
   if (!any(is.na(contract))) {
-    contract$bidder <- factor(contract$bidder, levels = contract$bidder[order(contract$amount, decreasing = TRUE)])
+    contract$bidder <- factor(contract$bidder, levels = contract$bidder[order(contract$amount, decreasing = FALSE)])
     contract.url <- contract[1,'contract']
     contract.number <- sub('.*/', '', contract.url)
     p <- ggplot(contract) +
@@ -13,8 +13,9 @@ plot.contract <- function(contract) {
       ggtitle(contract.url) + xlab('Name of bidder') +
       scale_y_continuous('Amount bid (in the labeled currency)', labels = comma) +
       theme(legend.position = 'top') +
+      scale_fill_manual(values = c('#00FFFF', '#666666', '#FF0000')) +
       geom_bar(stat = 'identity') + geom_text(hjust = 0) + coord_flip()
-    ggsave(filename = paste0(contract.number, '.png'), plot = p)
+    ggsave(filename = paste0('lowest-bidder/', contract.number, '.png'), plot = p)
   }
 }
 
@@ -23,9 +24,10 @@ main <- function() {
   bids.csv <- if (length(argv) == 0) 'bids.csv' else argv[1]
   bids <- read.csv(bids.csv, colClasses = c('character', 'character', 'factor', 'numeric', 'character'))
   bids$status <- factor(bids$status, levels = c('Awarded','Evaluated','Rejected'))
-  pdf('lowest-bidder.pdf', width = 11, height = 8.5)
+  if (!file.exists('lowest-bidder')) {
+    dir.create('lowest-bidder')
+  }
   d_ply(bids, 'contract', plot.contract)
-  dev.off()
 }
 
-# main()
+main()
