5f6938d346aa7a5c87057c8c07852f4e9af71025
Thomas Levine <_@thomaslevine.com>
2014-06-17 21:29:13 -0400

    NAs

--- redflags.r
+++ redflags.r
@@ -4,21 +4,24 @@ library(ggplot2)
 library(scales)
 
 plot.contract <- function(contract) {
-  p <- ggplot(contract) +
-    aes(x = bidder, y = amount, label = currency, fill = status) +
-    ggtitle(contract[1,'contract']) + xlab('Name of bidder') +
-    scale_y_continuous('Amount bid (in the labeled currency)', labels = comma) +
-    theme(legend.position = 'top') +
-    geom_bar(stat = 'identity') + geom_text(hjust = 0) + coord_flip()
-  print(p)
+  if (!any(is.na(contract))) {
+    p <- ggplot(contract) +
+      aes(x = bidder, y = amount, label = currency, fill = status) +
+      ggtitle(contract[1,'contract']) + xlab('Name of bidder') +
+      scale_y_continuous('Amount bid (in the labeled currency)', labels = comma) +
+      theme(legend.position = 'top') +
+      geom_bar(stat = 'identity') + geom_text(hjust = 0) + coord_flip()
+    print(p)
+  }
 }
 
 main <- function() {
-  bids.csv <- commandArgs(trailingOnly = TRUE)[1]
+  argv <- commandArgs(trailingOnly = TRUE)
+  bids.csv <- if (length(argv) == 0) 'bids.csv' else argv[1]
   bids <- read.csv(bids.csv, colClasses = c('character', 'character', 'factor', 'numeric', 'factor'))
   pdf('lowest-bidder.pdf', width = 11, height = 8.5)
   d_ply(bids, 'contract', plot.contract)
   dev.off()
 }
 
-#main()
+main()
