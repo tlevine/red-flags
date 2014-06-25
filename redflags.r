817d2be9c1c664ca64a7ea6459cc0a5cce0839fe
Thomas Levine <_@thomaslevine.com>
2014-06-25 19:15:40 +0000

    order

--- redflags.r
+++ redflags.r
@@ -100,15 +100,16 @@ main <- function() {
   roundness <- very.round(bids)
   roundness$contract.url <- paste0('http://search.worldbank.org/wcontractawards/procdetails/', roundness$contract.number)
   write.csv(roundness, 'roundness.csv', row.names = FALSE)
+  ggsave(filename = 'big/bid-patterns.png', plot = plot.bid.patterns(bids),
+         width = 11, height = 8.5, units = 'in', dpi = 300)
+
+  contracts <- ddply(bids, 'contract', is.lowest.bidder)
   write.csv(
     contracts[order(-as.numeric(contracts$n.rejected), as.numeric(contracts$n.evaluated)),][1:20,],
     'rejections.csv', row.names = FALSE)
-  contracts <- ddply(bids, 'contract', is.lowest.bidder)
   ggsave(filename = 'big/lowest-bidder.png', plot = plot.lowest.bidder(contracts),
          width = 11, height = 8.5, units = 'in', dpi = 300)
 # d_ply(bids, 'contract', plot.contract)
-  ggsave(filename = 'big/bid-patterns.png', plot = plot.bid.patterns(bids),
-         width = 11, height = 8.5, units = 'in', dpi = 300)
 }
 
 # main()
