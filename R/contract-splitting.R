9f6caf624da70f690a195f0955348e20a652a9c9
Thomas Levine <_@thomaslevine.com>
2014-07-08 10:26:10 -0400

    finding thresholds

--- R/contract-splitting.R
+++ R/contract-splitting.R
@@ -1,12 +1,22 @@
 #' Look for contract splitting, that is, repeated awards
 #' below procurement thresholds, possibly to the same contractor.
 #' 
-
+library(plyr)
 
 splitting <- function(contracts) {
   contracts$method.procurement <- factor(sub(' ?- ?[a-zA-Z].*', '', as.character(contracts$method.procurement)))
   contracts$method.selection <- factor(sub('.*([A-Z]{3,4}).*', '\\1', as.character(contracts$method.selection)))
-  contracts
+  contracts$method.procurement 
+
+  d_ply(contracts, 'project', plot.project)
 }
 
-ggplot(subset(contracts, project == 'P082604')) + aes(x = price.amount, fill = method.procurement) + facet_wrap(~ price.currency, scales = 'free') + geom_histogram() + scale_x_log10('Price', label = comma)
+plot.project <- function(project) {
+  project.name <- project[1,'project']
+  p <- ggplot(project) +
+    aes(x = price.amount, fill = method.procurement) +
+    facet_wrap(~ price.currency, scales = 'free') +
+    geom_histogram() + scale_x_log10('Price', label = comma) +
+    ggtitle(project.name)
+  ggsave(filename = paste0('outputs/splitting/', project.name, '.png'), plot = p)
+}
