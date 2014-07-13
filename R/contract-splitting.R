fe064bf7e8f2955f7b7a505a1f2fa9a3adac0396
Thomas Levine <_@thomaslevine.com>
2014-07-12 23:44:59 -0400

    annontate

--- R/contract-splitting.R
+++ R/contract-splitting.R
@@ -43,16 +43,21 @@ strange.prices <- function(contracts) {
   df
 }
 
+#' Plot a histogram of prices for a project.
+#'
 #' This isn't used in the detector, but you can use it like so.
 #' 
 #'   d_ply(contracts, 'project', plot.project)
 #'
-plot.project <- function(project) {
+#' @param project Data frame of contract-level data for one project
+#' @param dir The directory in which to save the plot.
+#' @return NULL
+plot.project <- function(project, dir = 'outputs/splitting/') {
   project.name <- project[1,'project']
   p <- ggplot(project) +
     aes(x = price.amount, fill = method.procurement) +
     facet_wrap(~ price.currency, scales = 'free') +
     geom_histogram() + scale_x_log10('Price', label = comma) +
     ggtitle(project.name)
-  ggsave(filename = paste0('outputs/splitting/', project.name, '.png'), plot = p)
+  ggsave(filename = paste0(dir, project.name, '.png'), plot = p)
 }
