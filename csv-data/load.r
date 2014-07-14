11c6f20e6bab3556d3968dcbf9b60d8a2bb2398e
Thomas Levine <_@thomaslevine.com>
2014-07-14 16:40:16 -0400

    rename

--- csv-data/load.r
+++ csv-data/load.r
@@ -1,8 +1,8 @@
-load.contracts.csv <- function(fn = 'data/contracts.csv') {
+load.contracts.csv <- function(fn = 'contracts.csv') {
   read.csv(fn, colClasses = c('factor','character','character','character','numeric','factor','numeric'))
 }
 
-load.bids.csv <- function(fn = 'data/bids.csv') {
+load.bids.csv <- function(fn = 'bids.csv') {
   bids <- read.csv(fn, colClasses = c('character', 'character', 'factor', 'numeric', 'character'))
   bids$contract.number <- sub('.*/', '', bids$contract)
   bids$status <- factor(bids$status, levels = c('Awarded','Evaluated','Rejected'))
