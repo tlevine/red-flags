2e8ebe5644a70cc1015abae9ffccf8480dd0fb2a
Thomas Levine <_@thomaslevine.com>
2015-06-09 15:58:15 +0000

    update to new schema

--- csv-data/update.r
+++ csv-data/update.r
@@ -3,8 +3,15 @@ load.contracts.csv <- function(fn = 'contracts.csv') {
 }
 
 load.bids.csv <- function(fn = 'bids.csv') {
-  bids <- read.csv(fn, colClasses = c('character', 'character', 'factor', 'numeric', 'character'))
+  bids <- read.csv(fn, colClasses = c('factor', 'factor', 'character', 'factor', 'factor',
+                                      'factor', 'numeric', 'character'))
   bids$contract.number <- sub('.*/', '', bids$contract)
   bids$status <- factor(bids$status, levels = c('Awarded','Evaluated','Rejected'))
   bids
 }
+
+contracts <- load.contracts.csv()
+bids <- load.bids.csv()
+
+save(contracts, file = '../data/contracts.RData')
+save(bids, file = '../data/bids.RData')
