55e7f38bfbedd0ce7af6f510158481544c302be0
Thomas Levine <_@thomaslevine.com>
2015-06-10 23:26:00 +0000

    RData

--- csv-data/update.r
+++ csv-data/update.r
@@ -4,6 +4,8 @@ load.contracts.csv <- function(fn = 'contracts.csv') {
 
 load.bids.csv <- function(fn = 'bids.csv') {
   bids <- read.csv(fn, colClasses = c('factor', 'factor', 'character', 'factor', 'factor',
+                                      'factor', 'numeric', 'character',
+                                      'factor', 'numeric', 'character',
                                       'factor', 'numeric', 'character'))
   bids$contract.number <- sub('.*/', '', bids$contract)
   bids$status <- factor(bids$status, levels = c('Awarded','Evaluated','Rejected'))
