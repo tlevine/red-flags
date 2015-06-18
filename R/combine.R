a229725d17d6e19089c3abd54b9e88c5959ca0f4
Thomas Levine <_@thomaslevine.com>
2015-06-18 09:14:02 +0000

    j

--- R/combine.R
+++ R/combine.R
@@ -10,19 +10,26 @@ join.contracts <- function(contracts.valuechange,
   
   df <- contracts[c('contract.country', 'contract', 'project')]
   for (name in names(contract.level)) {
-    contract.level[[name]][,contract.level] <- df$suspiciousness.score
-    df <- merge(df, contract.level[[name]][c('contract', 'suspiciousness.score')])
+    sub.df <- contract.level[[name]]
+    sub.df[name] <- sub.df$suspiciousness.score
+    df <- merge(df, sub.df[c('contract', name)])
   }
   df
 }
 
 join.projects <- function(contracts.valuechange, contracts.roundness,
-                             contracts.rejections, projects.prices) {
+                          contracts.rejections, projects.prices) {
   contract.flags <- join.contracts(contracts.valuechange,
                                    contracts.roundness,
                                    contracts.rejections)
   f <- function(df)
     sapply(df[c('valuechange', 'roundness', 'rejections')], max)
   project.flags <- ddply(contract.flags, 'project', f)
-  merge(project.flags, projects.prices[c('project', 'suspiciousness.score')])
+  projects.prices$price.kurtosis <- projects.prices$suspiciousness.score
+  merge(project.flags, projects.prices[c('project', 'price.kurtosis')])
+}
+
+model.projects <- function(projects.joined) {
+  numbers <- c('valuechange', 'roundness', 'rejections', 'price.kurtosis')
+  df <- projects.joined[numbers]
 }
